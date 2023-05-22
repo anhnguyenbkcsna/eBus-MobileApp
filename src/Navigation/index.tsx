51efa59e4d88daf960a7aa6ce86855e9d08f4071
Phu <phamhphu22@gmail.com>
2023-05-22 15:31:53 +0700

    Onboarding_complete

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -3,12 +3,14 @@ import { StatusBar } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigator } from "./Main";
-import { WelcomeContainer } from "@/Screens/Welcome";
+import { WelcomeContainer, Welcome1Container, Welcome2Container } from "@/Screens/Welcome";
 import { RootScreens } from "@/Screens";
 
 export type RootStackParamList = {
   [RootScreens.MAIN]: undefined;
   [RootScreens.WELCOME]: undefined;
+  [RootScreens.WELCOME1]: undefined;
+  [RootScreens.WELCOME2]: undefined;
 };
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
@@ -22,6 +24,14 @@ const ApplicationNavigator = () => {
         <RootStack.Screen
           name={RootScreens.WELCOME}
           component={WelcomeContainer}
+        />
+        <RootStack.Screen
+          name={RootScreens.WELCOME1}
+          component={Welcome1Container}
+        />
+        <RootStack.Screen
+          name={RootScreens.WELCOME2}
+          component={Welcome2Container}
         />
         <RootStack.Screen
           name={RootScreens.MAIN}
